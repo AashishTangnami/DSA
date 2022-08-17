406f816361b29493b1a5812e4eaa236f83190d37
Aashish Tangnami <tangnamiaashish1423@gmail.com>
2022-08-17 17:38:49 -0400

    Added pseudocode

--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,4 +1,13 @@
 # selection sorting 
+# select the smallest element from an unsorted list in each iteration.
+# place the smallest element at the begining of the unsorted list.
+
+""" Pseudocode
+1. compare minimum with second element.
+2. If the second element is smaller than first element, assign second element as minimum
+3. Compare minimum with third element as so on, iterate the process till the last element.
+4. After each iteration, minimum is placed in the front of the unsorted list.
+"""
 
 def select(seq: list, start: int)-> int:
     
