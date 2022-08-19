e3e94f5451da899e5cf798bb3735aa159690312f
Aashish Tangnami <tangnamiaashish1423@gmail.com>
2022-08-19 14:58:00 -0400

    docstring and code for bubble sort

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -9,5 +9,22 @@
 4. Iterate the process untill last element.
 
 """
-def bubble_sort():
-    return 'bubble sort'
+def bubble_sort(lst):
+    # Access each element in the list
+    for i in range(len(lst)):
+        # compare elements in the list
+        for j in range(0, len(lst)-i-1):
+            # compare two adjacent elements.
+            # change > to < to sort in descending order.
+            if lst[j] > lst[j + 1]: # ascending order
+                tmp_lst = lst[j]
+                lst[j] = lst[j+1] # swapping elements
+                lst[j+1] = tmp_lst 
+
+    return lst
+
+
+seq = [5,8,2,6,9,1,0,7]
+print(f'Before bubble sorting: {seq}')
+sorted_seq = bubble_sort(seq)
+print(f'After bubble sorting: {sorted_seq}')
