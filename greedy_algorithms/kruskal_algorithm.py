90201c98150d02626e58613c3228785436bd692a
Aashish Tangnami <tangnamiaashish1423@gmail.com>
2022-09-03 13:46:48 -0400

    kruskal algorithm

---
+++ greedy_algorithms/kruskal_algorithm.py
@@ -0,0 +1,68 @@
+# Kruskal algorithm is a minimum spanning tree algorithm,
+# that takes a graph as input and finds the subset of the edges of that graph which:
+# form a tree that includes every vertex.
+# has the minimum sum of weights among all the trees that can be formed from the graph.
+
+
+class Graph:
+    def __init__(self, vertices) -> None:
+        self.V = vertices
+        self.graph = []
+
+    def add_edge(self, u, v, w):
+        self.graph.append([u, v, w])
+
+    def search(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.search(parent, parent[i])
+    
+    def union(self, parent, rank, x, y):
+        x_root = self.search(parent, x)
+        y_root = self.search(parent, y)
+        if rank[x_root] < rank[y_root]:
+            parent[x_root] = y_root
+        elif rank[x_root] > rank[y_root]:
+            parent[y_root] = x_root
+        else:
+            parent[y_root] = x_root
+            rank[x_root] += 1
+
+    def kruskal_algorithm(self):
+        result = []
+        i, e = 0, 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i = i + 1
+            x = self.search(parent, u)
+            y = self.search(parent, v)
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.union(parent, rank, x, y)
+        for u, v, weight in result:
+            print("%d - %d: %d" % (u, v, weight))
+
+g = Graph(6)
+g.add_edge(0, 1, 4)
+g.add_edge(0, 2, 4)
+g.add_edge(1, 2, 2)
+g.add_edge(1, 0, 4)
+g.add_edge(2, 0, 4)
+g.add_edge(2, 1, 2)
+g.add_edge(2, 3, 3)
+g.add_edge(2, 5, 2)
+g.add_edge(2, 4, 4)
+g.add_edge(3, 2, 3)
+g.add_edge(3, 4, 3)
+g.add_edge(4, 2, 4)
+g.add_edge(4, 3, 3)
+g.add_edge(5, 2, 2)
+g.add_edge(5, 4, 3)
+g.kruskal_algorithm()
