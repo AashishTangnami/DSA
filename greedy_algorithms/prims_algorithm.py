59bdf27c97a97d035a84c13cedf159cbe1a0f563
Aashish Tangnami <tangnamiaashish1423@gmail.com>
2022-09-16 11:34:10 -0400

    added comments

--- greedy_algorithms/prims_algorithm.py
+++ greedy_algorithms/prims_algorithm.py
@@ -9,9 +9,31 @@
 no_edge = 0 # initial val
 V =  5 # number of vertices 
 inf = 99999999 
-selected = []
-G = []
+
+
+selected = [0, 0, 0, 0, 0]
+# the number of egde in minimum spanning tree will be
+# always less than(V - 1), where V is number of vertices in
+# graph
+# choose 0th vertex and make it true
+selected[0] = True
+# print for edge and weight
+print("Edge : Weight\n")
+
+# 2d array - matrix of 5X5
+# for adjacent matrix to represent graph
+
+G = [[0, 9, 75, 0, 0],
+     [9, 0, 95, 19, 42],
+     [75, 95, 0, 51, 66],
+     [0, 19, 51, 0, 31],
+     [0, 42, 66, 31, 0]]
+
 while (no_edge < V - 1):
+    # For every vertex in the set S, find the all adjacent vertices
+    #, calculate the distance from the vertex selected at step 1.
+    # if the vertex is already in the set S, discard it otherwise
+    # choose another vertex nearest to selected vertex  at step 1.
     minimum = inf
     x = 0
     y = 0
