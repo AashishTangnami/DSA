49bcf4dcd6d94da67451f25aba3daaf3f8c5548f
Aashish Tangnami <aashishtangnami@Aashishs-MacBook-Air.local>
2022-11-15 02:51:53 -0500

    merging

--- searching/dfs.py
+++ searching/dfs.py
@@ -1,10 +1,11 @@
 def dfs(graph, start, visited=None):
+    # depth first search 
     if visited is None:
         visited = set()
     visited.add(start)
 
     print(start)
-    
+
     for next in graph[start] - visited :
         dfs(graph, next, visited)
     return visited
