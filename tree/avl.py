574893f4398969cec9d67b382228d609b47c90ae
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2023-02-23 14:03:00 -0500

    avl tree

---
+++ tree/avl.py
@@ -0,0 +1,168 @@
+# AVL tree implementation in Python
+
+
+import sys
+
+# Create a tree node
+class TreeNode(object):
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+
+class AVLTree(object):
+
+    # Function to insert a node
+    def insert_node(self, root, key):
+
+        # Find the correct location and insert the node
+        if not root:
+            return TreeNode(key)
+        elif key < root.key:
+            root.left = self.insert_node(root.left, key)
+        else:
+            root.right = self.insert_node(root.right, key)
+
+        root.height = 1 + max(self.get_height(root.left),
+                              self.get_height(root.right))
+
+        # Update the balance factor and balance the tree
+        balance_factor = self.get_balance(root)
+        if balance_factor > 1:
+            if key < root.left.key:
+                return self.right_rotate(root)
+            else:
+                root.left = self.left_rotate(root.left)
+                return self.right_rotate(root)
+
+        if balance_factor < -1:
+            if key > root.right.key:
+                return self.left_rotate(root)
+            else:
+                root.right = self.right_rotate(root.right)
+                return self.left_rotate(root)
+
+        return root
+
+    # Function to delete a node
+    def delete_node(self, root, key):
+
+        # Find the node to be deleted and remove it
+        if not root:
+            return root
+        elif key < root.key:
+            root.left = self.delete_node(root.left, key)
+        elif key > root.key:
+            root.right = self.delete_node(root.right, key)
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+            temp = self.get_min_value_node(root.right)
+            root.key = temp.key
+            root.right = self.delete_node(root.right,
+                                          temp.key)
+        if root is None:
+            return root
+
+        # Update the balance factor of nodes
+        root.height = 1 + max(self.get_height(root.left),
+                              self.get_height(root.right))
+
+        balance_factor = self.get_balance(root)
+
+        # Balance the tree
+        if balance_factor > 1:
+            if self.get_balance(root.left) >= 0:
+                return self.right_rotate(root)
+            else:
+                root.left = self.left_rotate(root.left)
+                return self.right_rotate(root)
+        if balance_factor < -1:
+            if self.get_balance(root.right) <= 0:
+                return self.left_rotate(root)
+            else:
+                root.right = self.right_rotate(root.right)
+                return self.left_rotate(root)
+        return root
+
+    # Function to perform left rotation
+    def left_rotate(self, z):
+        y = z.right
+        T2 = y.left
+        y.left = z
+        z.right = T2
+        z.height = 1 + max(self.get_height(z.left),
+                           self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left),
+                           self.get_height(y.right))
+        return y
+
+    # Function to perform right rotation
+    def right_rotate(self, z):
+        y = z.left
+        T3 = y.right
+        y.right = z
+        z.left = T3
+        z.height = 1 + max(self.get_height(z.left),
+                           self.get_height(z.right))
+        y.height = 1 + max(self.get_height(y.left),
+                           self.get_height(y.right))
+        return y
+
+    # Get the height of the node
+    def get_height(self, root):
+        if not root:
+            return 0
+        return root.height
+
+    # Get balance factore of the node
+    def get_balance(self, root):
+        if not root:
+            return 0
+        return self.get_height(root.left) - self.get_height(root.right)
+
+    def get_min_value_node(self, root):
+        if root is None or root.left is None:
+            return root
+        return self.get_min_value_node(root.left)
+
+    def pre_order(self, root):
+        if not root:
+            return
+        print("{0} ".format(root.key), end="")
+        self.pre_order(root.left)
+        self.pre_order(root.right)
+
+    # Print the tree
+    def print_helper(self, curr_ptr, indent, last):
+        if curr_ptr != None:
+            sys.stdout.write(indent)
+            if last:
+                sys.stdout.write("R----")
+                indent += "     "
+            else:
+                sys.stdout.write("L----")
+                indent += "|    "
+            print(curr_ptr.key)
+            self.print_helper(curr_ptr.left, indent, False)
+            self.print_helper(curr_ptr.right, indent, True)
+
+
+myTree = AVLTree()
+root = None
+nums = [33, 13, 52, 9, 21, 61, 8, 11]
+for num in nums:
+    root = myTree.insert_node(root, num)
+myTree.print_helper(root, "", True)
+key = 13
+root = myTree.delete_node(root, key)
+print("After Deletion: ")
+myTree.print_helper(root, "", True)
