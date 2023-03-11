a0443642d426cefe35ba3c76f876bd6ba0412aba
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2023-03-10 23:58:01 -0500

    modified codes

--- sorting/bucket_sort.py
+++ sorting/bucket_sort.py
@@ -1,34 +1,33 @@
-# Bucket sorting divides the unsorted array elements into several groups called buckets.
-# Scatter gather approach is used  in the bucket sort process.
-# complexity
-    # best - O(n+k)
-    # worst - O(n^2)
-    # avg - O(n)
-def bucket_sort(seq):
-    bucket = []
+from typing import List
 
+def bucket_sort(seq: List[float]) -> List[float]:
+    """
+    Sorts a list of floats using the Bucket Sort algorithm.
+    
+    complexity
+        best - O(n+k)
+        worst - O(n^2)
+        avg - O(n)
+    """
+    n = len(seq)
     # Create empty buckets
-    for i in range(len(seq)):
-        bucket.append([])
+    bucket = [[] for _ in range(n)]
 
     # Insert elements into their respective buckets
     for j in seq:
-        idx_b = int(10 * j)
+        idx_b = int(n * j)
         bucket[idx_b].append(j)
 
     # Sort the elements of each bucket
-    for i in range(len(seq)):
-        bucket[i] = sorted(bucket[i])
+    for i in range(n):
+        bucket[i].sort()
 
     # Get the sorted elements
-    k = 0
-    for i in range(len(seq)):
-        for j in range(len(bucket[i])):
-            seq[k] = bucket[i][j]
-            k += 1
-    return seq
+    sorted_seq = [elem for bucket_list in bucket for elem in bucket_list]
 
+    return sorted_seq
 
+# Example usage
 seq = [.42, .32, .33, .52, .37, .47, .51]
 print(f'Unsorted list : {seq}')
 print(f"Sorted list in ascending order is : {bucket_sort(seq)}")
--- sorting/heap_sort.py
+++ sorting/heap_sort.py
@@ -1,37 +1,55 @@
-# Heap Sort in python
+from typing import List
 
-def heap(seq, n, i):
-    large_val = i
-    left = 2 * i + 1
-    right = 2 * i + 2
+def heapify(seq: List[int], n: int, i: int) -> None:
+    """
+    Heapify the subtree rooted at index i in seq.
 
-    if left < n and seq[i] < seq[left]:
-        large_val = left
-  
-    if right < n and seq[large_val] < seq[right]:
-        large_val = right
+    Args:
+    - seq: List of integers to be sorted
+    - n: Size of the heap
+    - i: Index of the subtree to be heapified
+    """
+    largest = i  # Initialize largest as root
+    left = 2 * i + 1  # left child index
+    right = 2 * i + 2  # right child index
 
-    # If root is not largest, swap with largest and continue heapifying
-    if large_val != i:
-        seq[i], seq[large_val] = seq[large_val], seq[i]
-        heap(seq, n, large_val)
+    # If left child is larger than root
+    if left < n and seq[largest] < seq[left]:
+        largest = left
 
-def heap_sort(seq):
+    # If right child is larger than largest so far
+    if right < n and seq[largest] < seq[right]:
+        largest = right
+
+    # If largest is not root
+    if largest != i:
+        seq[i], seq[largest] = seq[largest], seq[i]  # swap
+        heapify(seq, n, largest)
+
+def heap_sort(seq: List[int]) -> List[int]:
+    """
+    Sort the given list of integers using heap sort algorithm.
+
+    Args:
+    - seq: List of integers to be sorted
+
+    Returns:
+    - Sorted list of integers
+    """
     n = len(seq)
-    for i in range(n//2, -1, -1):
-        heap(seq, n, i)
-    
-    for i in range(n-1, 0, -1):
-        # Swap
-        seq[i], seq[0] = seq[0], seq[i]
-  
-        # Heapify root element
-        heap(seq, i, 0)
+
+    # Build a max heap
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(seq, n, i)
+
+    # Extract elements one by one
+    for i in range(n - 1, 0, -1):
+        seq[i], seq[0] = seq[0], seq[i]  # swap
+        heapify(seq, i, 0)
+
+    return seq
 
 seq = [5, 8, 2, 6, 9, 1, 0, 7]
-heap_sort(seq)
-n = len(seq)
-print("Sorted array is")
-for i in range(n):
-    print("%d " % seq[i], end='')
-  
+print(f"Unsorted list: {seq}")
+sorted_seq = heap_sort(seq)
+print(f"Sorted list: {sorted_seq}")
