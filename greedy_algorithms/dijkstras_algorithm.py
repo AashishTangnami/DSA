6cd218acbd5b163942ea9217e6bd5889c89f6e00
Aashish Tangnami <42236161+AashishTangnami@users.noreply.github.com>
2023-03-12 23:34:05 -0400

    updated code

--- greedy_algorithms/dijkstras_algorithm.py
+++ greedy_algorithms/dijkstras_algorithm.py
@@ -1,73 +1,55 @@
-
-# Dijkstra's Algorithm allows us to find,
-# the shortest path between any two vertices of a graph.
-
-# Shortest path might not include all the vertices of a graph.
-
-# Pseudocode.
-    # function dijkstra(g, s)
-        #for each vertex V in graph
-            # distance[V] <- infinite
-            # previous[V] <- Null
-            # if V != S, add V to priority Queue Q
-        # distance[S] < 0
-    # While Q is not empty
-        # U <- Extract MIN from Q
-        # for each unvisited neighbor V of U 
-            # temp_distance <- distance[U] + edge_weight(U, V)
-            # if temp_distance < distance[V]
-                # distance[V] <- temp_distance
-                # previous[V] <- U
-    # return distance[], previous[]
-
 import sys
 
-vertices = [[0, 0, 1, 1, 0, 0, 0],
-            [0, 0, 1, 0, 0, 1, 0],
-            [1, 1, 0, 1, 1, 0, 0],
-            [1, 0, 1, 0, 0, 0, 1],
-            [0, 0, 1, 0, 0, 1, 0],
-            [0, 1, 0, 0, 1, 0, 1],
-            [0, 0, 0, 1, 0, 1, 0]]
-            
-edges = [[0, 0, 1, 2, 0, 0, 0],
-         [0, 0, 2, 0, 0, 3, 0],
-         [1, 2, 0, 1, 3, 0, 0],
-         [2, 0, 1, 0, 0, 0, 1],
-         [0, 0, 3, 0, 0, 2, 0],
-         [0, 3, 0, 0, 2, 0, 1],
-         [0, 0, 0, 1, 0, 1, 0]]
+# Define vertices and edges as lists of lists containing integers
+vertices: list[list[int]] = [[0, 0, 1, 1, 0, 0, 0],
+                             [0, 0, 1, 0, 0, 1, 0],
+                             [1, 1, 0, 1, 1, 0, 0],
+                             [1, 0, 1, 0, 0, 0, 1],
+                             [0, 0, 1, 0, 0, 1, 0],
+                             [0, 1, 0, 0, 1, 0, 1],
+                             [0, 0, 0, 1, 0, 1, 0]]
+edges: list[list[int]] = [[0, 0, 1, 2, 0, 0, 0],
+                          [0, 0, 2, 0, 0, 3, 0],
+                          [1, 2, 0, 1, 3, 0, 0],
+                          [2, 0, 1, 0, 0, 0, 1],
+                          [0, 0, 3, 0, 0, 2, 0],
+                          [0, 3, 0, 0, 2, 0, 1],
+                          [0, 0, 0, 1, 0, 1, 0]]
 
-num_of_vertices = len(vertices[0])
-def to_be_visited():
-    global visited_and_distance
-    v = - 10
-    for index in range(num_of_vertices):
-        if visited_and_distance[index][0] == 0 \
-            and (v < 0 or visited_and_distance[index][1] <= \
-                visited_and_distance[v][1]):
-            v = index
-    return v
+num_of_vertices: int = len(vertices[0])
 
-visited_and_distance = [[0,0]]
+def to_be_visited() -> int:
+    """Returns the index of the vertex with the smallest tentative distance."""
+    global visited_and_distance  # declare global variable
+    v: int = -10  # initialize v to a negative value
+    for index in range(num_of_vertices):  # iterate over all vertices
+        if visited_and_distance[index][0] == 0 and (v < 0 or visited_and_distance[index][1] <= visited_and_distance[v][1]):
+            # if the vertex has not been visited and it has the smallest tentative distance so far
+            v = index  # update the index of the vertex with the smallest tentative distance
+    return v  # return the index of the vertex with the smallest tentative distance
 
-for i in range(num_of_vertices-1):
+
+# Initialize visited_and_distance as a list of lists containing 2 integers each
+visited_and_distance: list[list[int]] = [[0, 0]]
+for i in range(num_of_vertices - 1):
     visited_and_distance.append([0, sys.maxsize])
 
+# Apply Dijkstra's algorithm
 for vertex in range(num_of_vertices):
-    to_visit = to_be_visited()
-    
+    to_visit: int = to_be_visited()  # Get the vertex with the smallest tentative distance
     for neighbour_index in range(num_of_vertices):
-        if vertices[to_visit][neighbour_index] == 1 and \
-            visited_and_distance[neighbour_index][0] == 0:
-            new_distance = visited_and_distance[to_visit][1] + edges[to_visit][neighbour_index]
+        # If the vertex is adjacent and not visited
+        if vertices[to_visit][neighbour_index] == 1 and visited_and_distance[neighbour_index][0] == 0:
+            # Calculate the new tentative distance
+            new_distance: int = visited_and_distance[to_visit][1] + edges[to_visit][neighbour_index]
+            # If the new tentative distance is smaller, update the distance
             if visited_and_distance[neighbour_index][1] > new_distance:
                 visited_and_distance[neighbour_index][1] = new_distance
-
+        # Mark the current vertex as visited
         visited_and_distance[to_visit][0] = 1
-i = 0
 
+# Print the distances
+i: int = 0
 for distance in visited_and_distance:
-    print("Distance of ", chr(ord('a') + i),
-          " from source vertex: ", distance[1])
+    print(f"Distance of {chr(ord('a') + i)} from source vertex: {distance[1]}")
     i = i + 1
